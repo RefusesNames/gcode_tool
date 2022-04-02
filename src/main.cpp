c0cf990c5e21cfa41b06bb9411451e9a7bca2de2
Jerome Reinlaender <jerome.reinlaender@gmail.com>
2022-04-02 20:52:08 +0200

    Basic navigation works with the mouse

--- src/main.cpp
+++ src/main.cpp
@@ -12,14 +12,34 @@
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
-void error_callback(int, const char* error_message);
+void error_callback(int, const char * error_message);
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+
+void mouse_position_callback(GLFWwindow * window, double x_position, double y_position);
+
+void scroll_callback(GLFWwindow * window, double x_offset, double y_offset);
 
 GLenum glCheckError_(const char *file, int line);
 #define glCheckError() glCheckError_(__FILE__, __LINE__)
 
-std::vector<glm::vec3> commands_to_vertices(std::vector<gcode::command> const & commands);
+std::vector<glm::vec3> commands_to_vertices(std::vector<gcode::command> & commands);
 
 auto camera_position = glm::vec3(0.0f, 0.0f, -3.0f);
+
+auto object_translation = glm::vec3(0.0f, 0.0f, 0.0f);
+
+struct mouse_information
+{
+    glm::vec2 position = glm::vec2(0.0f, 0.0f);
+    bool left_button_pressed = false;
+    bool right_button_pressed = false;
+    bool middle_button_pressed = false;
+    float scroll_value = 0.0f;
+} mouse;
+
+void zoom_out();
+void zoom_in();
 
 int main(int argc, char *argv[])
 {
@@ -38,6 +58,9 @@ int main(int argc, char *argv[])
     glfwMakeContextCurrent(window);
 
     glfwSetKeyCallback(window, key_callback);
+    glfwSetMouseButtonCallback(window, mouse_button_callback);
+    glfwSetCursorPosCallback(window, mouse_position_callback);
+    glfwSetScrollCallback(window, scroll_callback);
 
     gcode::reader reader;
     auto commands = reader.read_file(argv[1]);
@@ -118,9 +141,10 @@ int main(int argc, char *argv[])
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        auto model_matrix = glm::translate(glm::mat4(1.0f), object_translation);
         auto view_matrix = glm::lookAt(camera_position, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-        auto projection_matrix = glm::perspective(glm::radians(45.0f), (float) window_parameters.width / (float) window_parameters.height, 1.0f, 100.0f);
-        auto mvp_matrix = projection_matrix * view_matrix;
+        auto projection_matrix = glm::perspective(glm::radians(45.0f), (float) window_parameters.width / (float) window_parameters.height, 1.0f, 1000.0f);
+        auto mvp_matrix = projection_matrix * view_matrix * model_matrix;
         GLint matrix_uniform = glGetUniformLocation(shader_program, "model_view_projection_matrix");
         glUniformMatrix4fv(matrix_uniform, 1, GL_FALSE, glm::value_ptr(mvp_matrix));
 
@@ -145,10 +169,10 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
         glfwSetWindowShouldClose(window, GL_TRUE);
 
     if (key == GLFW_KEY_S && action == GLFW_PRESS)
-        camera_position.z--;
+        zoom_out();
         
     if (key == GLFW_KEY_W && action == GLFW_PRESS)
-        camera_position.z++;
+        zoom_in();
 
     if (key == GLFW_KEY_A && action == GLFW_PRESS)
         camera_position.x--;
@@ -184,11 +208,27 @@ GLenum glCheckError_(const char *file, int line)
     return errorCode;
 }
 
-std::vector<glm::vec3> commands_to_vertices(std::vector<gcode::command> const & commands)
+std::vector<glm::vec3> commands_to_vertices(std::vector<gcode::command> & commands)
 {
     glm::vec3 previous_location = glm::vec3(0, 0, 0);
     std::vector<glm::vec3> vertices;
     vertices.reserve(commands.size());
+
+    for(auto & command : commands)
+    {
+        for(auto & parameter : command.parameters)
+        {
+            if (parameter.name == 'Z')
+                parameter.name = 'Y';
+            else if (parameter.name == 'Y')
+            {
+                parameter.name = 'Z';
+                parameter.value *= -1.0f;
+            }
+            else if (parameter.name == 'X')
+                parameter.value *= -1.0f;
+        }
+    }
 
     for(auto command : commands)
     {
@@ -229,4 +269,48 @@ std::vector<glm::vec3> commands_to_vertices(std::vector<gcode::command> const & 
     }
 
     return vertices;
+}
+
+void zoom_out()
+{
+    camera_position.z--;
+}
+
+void zoom_in()
+{
+    camera_position.z++;
+}
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+        mouse.left_button_pressed = true;
+    else if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE)
+        mouse.left_button_pressed = false;
+    else if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
+        mouse.right_button_pressed = true;
+    else if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_RELEASE)
+        mouse.right_button_pressed = false;
+    else if (button == GLFW_MOUSE_BUTTON_MIDDLE && action == GLFW_PRESS)
+        mouse.middle_button_pressed = true;
+    else if (button == GLFW_MOUSE_BUTTON_MIDDLE && action == GLFW_RELEASE)
+        mouse.middle_button_pressed = false;
+
+}
+
+void mouse_position_callback(GLFWwindow * window, double x_position, double y_position)
+{
+    auto new_position = glm::vec2(x_position, y_position);
+    if (mouse.middle_button_pressed)
+    {
+        auto difference = new_position - mouse.position;
+        object_translation += glm::vec3(difference, 0);
+    }
+    mouse.position = new_position;
+}
+
+void scroll_callback(GLFWwindow * window, double x_offset, double y_offset)
+{
+    camera_position.z += static_cast<float>(y_offset);
+    mouse.scroll_value += static_cast<float>(y_offset);
 }
