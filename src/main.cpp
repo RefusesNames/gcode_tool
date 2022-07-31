c49b2680d8919c0cbc5c2fc55240735c023de71b
Jerome Reinlaender <jerome.reinlaender@gmail.com>
2022-07-31 12:06:06 +0200

    Add main.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,17 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
+#include "main.h"
+
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/string_cast.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <iostream>
 
 #include "window.h"
-#include "gcode_reader.h"
 #include "shaders.h"
 #include "camera.h"
 
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
-
-void error_callback(int, const char * error_message);
-
-void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
-
-void mouse_position_callback(GLFWwindow * window, double x_position, double y_position);
-
-void scroll_callback(GLFWwindow * window, double x_offset, double y_offset);
-
-GLenum glCheckError_(const char *file, int line);
-#define glCheckError() glCheckError_(__FILE__, __LINE__)
-
-struct axis_aligned_bounding_box
-{
-    glm::vec3 lower = glm::vec3();
-    glm::vec3 upper = glm::vec3();
-    glm::vec3 weighted_center = glm::vec3();
-};
-
-struct annotated_vertex
-{
-    glm::vec3 position;
-    uint32_t type;
-};
-
-std::vector<annotated_vertex> commands_to_vertices(std::vector<gcode::command> & commands);
-
-axis_aligned_bounding_box create_aabb(std::vector<annotated_vertex> const & vertices);
-
 camera camera(glm::vec3(0.0f, 0.0f, -3.0f), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.0, 1.0f, 0.0f));
 
-struct mouse_information
-{
-    glm::vec2 position = glm::vec2(0.0f, 0.0f);
-    bool left_button_pressed = false;
-    bool right_button_pressed = false;
-    bool middle_button_pressed = false;
-    float scroll_value = 0.0f;
-} mouse;
+mouse_information mouse;
 
 int main(int argc, char *argv[])
 {
---
+++ src/main.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <vector>
+
+#include "gcode_reader.h"
+
+struct axis_aligned_bounding_box
+{
+    glm::vec3 lower = glm::vec3();
+    glm::vec3 upper = glm::vec3();
+    glm::vec3 weighted_center = glm::vec3();
+};
+
+struct annotated_vertex
+{
+    glm::vec3 position;
+    uint32_t type;
+};
+
+struct mouse_information
+{
+    glm::vec2 position = glm::vec2(0.0f, 0.0f);
+    bool left_button_pressed = false;
+    bool right_button_pressed = false;
+    bool middle_button_pressed = false;
+    float scroll_value = 0.0f;
+};
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+void error_callback(int, const char * error_message);
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+
+void mouse_position_callback(GLFWwindow * window, double x_position, double y_position);
+
+void scroll_callback(GLFWwindow * window, double x_offset, double y_offset);
+
+GLenum glCheckError_(const char *file, int line);
+#define glCheckError() glCheckError_(__FILE__, __LINE__)
+
+std::vector<annotated_vertex> commands_to_vertices(std::vector<gcode::command> & commands);
+
+axis_aligned_bounding_box create_aabb(std::vector<annotated_vertex> const & vertices);
